0e345a01cc6fccaa25420c5bbcf15cda26e3e7cd
Santhiago Rodrigues Correa <rx192@xgen.local>
2018-05-22 19:12:10 -0300

    initial commit

---
+++ assets/app/messages/message-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+import { Message } from "./message.model";
+import { MessageService } from "./message.service";
+
+@Component({
+  selector: 'app-message-list',
+  templateUrl: './message-list.component.html',
+  styleUrls: ['message-list.component.css']
+})
+export class MessageListComponent implements OnInit {
+  messages: Message[];
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit() {
+    this.messageService.getMessages()
+      .subscribe(
+        (messages: Message[]) => {
+          this.messages = messages;
+        }
+      );
+  }
+}
